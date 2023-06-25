ac7956384e3b1e62e808d39bcac85a086b6191f6
Zenen Peraza <peraza@outlook.com>
2023-06-25 12:09:41 -0400

    six commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-=======
-
->>>>>>> d0f003e398d4347eb66463b7b89dd47fbb72cb7b
   <router-view/>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
-=======
 <ParentComponent></ParentComponent>
 </template>
 
@@ -24,5 +6,4 @@ export default defineComponent({
 import ParentComponent from '@/components/ParentComponent.vue';
 
 
->>>>>>> d0f003e398d4347eb66463b7b89dd47fbb72cb7b
 </script>
